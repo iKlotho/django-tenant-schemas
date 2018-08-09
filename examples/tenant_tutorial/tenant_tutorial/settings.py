89dad13995f18af5cb589abe75fa03e377f43e91
kaustubh <kaustubh@zelthy.com>
2018-08-09 12:18:59 +0530

    additional points

--- examples/tenant_tutorial/tenant_tutorial/settings.py
+++ examples/tenant_tutorial/tenant_tutorial/settings.py
@@ -15,8 +15,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'tenant_schemas.postgresql_backend',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'tenant_tutorial',                      # Or path to database file if using sqlite3.
-        'USER': 'myprojectuser',
-        'PASSWORD': 'password1',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
         'HOST': 'localhost',   # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',            # Set to empty string for default.
     }
